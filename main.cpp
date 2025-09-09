66056ad22485c94758a6d7f71187557dfb7e0ffd
thelastgoth128 <thindwacyrus86@gmail.com>
2025-09-09 14:23:24 +0200

    main function

--- main.cpp
+++ main.cpp
@@ -57,14 +57,13 @@ static void activate(GtkApplication *app, gpointer user_data) {
     gtk_window_present(GTK_WINDOW(window));
 }
 
-int main (int argc, char **argv) {
-    GtkApplication *app;
-    int status;
-
-    app = gtk_application_new("ModelLoader", G_APPLICATION_DEFAULT_FLAGS);
+int main(int argc, char **argv) {
+    GtkApplication *app = gtk_application_new("com.example.ModelLoader", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-    status = g_application_run(G_APPLICATION (app), argc, argv);
+
+    int status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
+    
 
     return status;
 }
