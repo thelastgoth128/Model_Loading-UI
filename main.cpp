a0962106629386a33956427b1b2892ec377c37ef
thelastgoth128 <thindwacyrus86@gmail.com>
2025-09-10 12:17:00 +0200

    added an overlay

--- main.cpp
+++ main.cpp
@@ -61,12 +61,48 @@ static void open_file(GtkWidget *widget, gpointer data) {
 static void activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Model Loader");
-    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 800);
 
-    GtkWidget *button = gtk_button_new_with_label("Browse");
-    g_signal_connect(button, "clicked", G_CALLBACK(open_file), NULL);
-    gtk_window_set_child(GTK_WINDOW(window), button);
-    
+    // Create overlay container
+    GtkWidget *overlay = gtk_overlay_new();
+    gtk_window_set_child(GTK_WINDOW(window), overlay);
+
+    // Background image
+    GtkWidget *background = gtk_picture_new_for_filename("C:/Users/HP/OneDrive/Documentos/Cyrus/Projects/model_loading_UI/tree.jpg");
+    gtk_widget_set_hexpand(background, TRUE);
+    gtk_widget_set_vexpand(background, TRUE);
+    gtk_overlay_set_child(GTK_OVERLAY(overlay), background);
+
+    // Centered label
+    GtkWidget *label = gtk_label_new("🧊 Load 3D Model");
+    gtk_widget_set_halign(label, GTK_ALIGN_CENTER);
+    gtk_widget_set_valign(label, GTK_ALIGN_CENTER);
+    gtk_overlay_add_overlay(GTK_OVERLAY(overlay), label);
+    gtk_widget_add_css_class(label, "title");
+
+    // Bottom-right Browse button
+    GtkWidget *buttonBox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
+    gtk_widget_set_halign(buttonBox, GTK_ALIGN_END);
+    gtk_widget_set_valign(buttonBox, GTK_ALIGN_END);
+
+    GtkWidget *browseButton = gtk_button_new_with_label("Browse");
+    g_signal_connect(browseButton, "clicked", G_CALLBACK(open_file), NULL);
+    gtk_box_append(GTK_BOX(buttonBox), browseButton);
+    gtk_overlay_add_overlay(GTK_OVERLAY(overlay), buttonBox);
+
+    // Optional: Spinner (bottom-left or center)
+    // GtkWidget *spinner = gtk_spinner_new();
+    // gtk_spinner_start(GTK_SPINNER(spinner));
+    // gtk_widget_set_halign(spinner, GTK_ALIGN_START);
+    // gtk_widget_set_valign(spinner, GTK_ALIGN_END);
+    // gtk_overlay_add_overlay(GTK_OVERLAY(overlay), spinner);
+
+    // Load CSS
+    GtkCssProvider *provider = gtk_css_provider_new();
+    gtk_css_provider_load_from_path(provider, "C:/Users/HP/OneDrive/Documentos/Cyrus/Projects/model_loading_UI/style.css");
+    gtk_style_context_add_provider_for_display(gdk_display_get_default(),
+        GTK_STYLE_PROVIDER(provider),
+        GTK_STYLE_PROVIDER_PRIORITY_USER);
 
     gtk_window_present(GTK_WINDOW(window));
 }
